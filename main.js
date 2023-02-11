7387c48b0ac191d5da6f3ac653eb51d817b2f438
renato <rprobstsussoni@gmail.com>
2023-02-10 19:03:02 -0500

    movies are rendered

--- main.js
+++ main.js
@@ -1,23 +1,71 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+const API_KEY = "apikey=af0dad7d&s=";
+const BASE_URL = "https://www.omdbapi.com/?";
+const API_URL = BASE_URL + API_KEY;
+const movies = [];
+const form = document.querySelector(".header__search-bar");
+form.addEventListener("submit", logSubmit);
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+async function request(movie) {
+  fetch(`${API_URL}${movie}`).then((response) =>
+    response.json().then((data) => {
+      renderMovies(data.Search);
+    })
+  );
+}
 
-setupCounter(document.querySelector('#counter'))
+function renderMovies(movies) {
+  cleanContainer();
+  movies.map((movie) => {
+    const poster = movie.Poster;
+    const title = movie.Title;
+    const year = movie.Year;
+    createCard(poster, title, year);
+  });
+}
+
+function logSubmit(event) {
+  event.preventDefault();
+  let formData = new FormData(form);
+
+  for (const input of formData.entries()) {
+    const title = input[1];
+    request(title);
+  }
+
+  form.reset();
+}
+
+function cleanContainer() {
+  const main = document.querySelector("main");
+  main.innerHTML = "";
+}
+
+function createCard(poster, movieTitle, movieYear) {
+  if (poster !== "N/A") {
+    const main = document.querySelector("main");
+
+    const section = document.createElement("section");
+    section.className = "main__movie-card";
+
+    const div = document.createElement("div");
+    div.className = "main__movie-card__poster";
+
+    const img = document.createElement("img");
+    img.src = poster;
+
+    const info = document.createElement("div");
+    info.className = "main__movie-card__info";
+
+    const title = document.createElement("h2");
+    title.textContent = movieTitle;
+    const year = document.createElement("h2");
+    year.textContent = movieYear;
+
+    main.appendChild(section);
+    section.appendChild(div);
+    div.appendChild(img);
+    section.appendChild(info);
+    info.appendChild(title);
+    info.appendChild(year);
+  }
+}
